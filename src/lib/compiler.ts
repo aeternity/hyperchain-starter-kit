3375768bfb9dc84365a1a1799a2c81a11fefd987
Metin Akat <akat.metin@gmail.com>
2022-08-17 21:20:26 +0300

    Helper module to call the http compiler directly

---
+++ src/lib/compiler.ts
@@ -0,0 +1,28 @@
+import { definitions, paths } from "./compilerApi";
+import axios, { AxiosResponse } from "axios";
+
+export const COMPILER_URL = "https://compiler.aeternity.io/";
+
+export async function getACI(source: string): Promise<definitions["ACI"]> {
+  const resp = await axios
+    .post<paths["/aci"], AxiosResponse<definitions["ACI"]>>(
+      `${COMPILER_URL}aci`,
+      {
+        code: source,
+        options: {},
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .catch((e) => {
+      console.log("error message", e.message);
+      console.log("error data", e.response.data);
+    });
+  if (!resp) {
+    throw new Error("Could not fetch ACI");
+  }
+  return resp.data;
+}
