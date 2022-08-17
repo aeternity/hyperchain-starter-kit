d40f2f89e7948110674b2cad45558f0366425a47
Metin Akat <akat.metin@gmail.com>
2022-08-17 21:21:52 +0300

    Basic working script that generates contract definitions

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,19 @@ import {
   colors,
   uniqueNamesGenerator,
 } from "unique-names-generator";
-// @ts-ignore
-import bip39 from "bip39";
 import aesdk from "@aeternity/aepp-sdk";
-const { getHdWalletAccountFromSeed } = aesdk;
+import axios from "axios";
+// import * as compilerApi from "./lib/compilerAPI.js";
+// import { Api, HttpClient } from "./lib/compilerAPI.js";
+// @ts-ignore
+import aecalldata from "@aeternity/aepp-calldata";
+import { AccountWithSecrets, genAccount } from "./lib/basicTypes.js";
+import { genContractDef, OWNER_ADDR } from "./lib/contracts.js";
+
+const STAKING_VALIDATOR_SOURCE_URL =
+  "https://raw.githubusercontent.com/aeternity/aeternity/master/test/contracts/StakingValidator.aes";
+const MAIN_STAKING_SOURCE_URL =
+  "https://raw.githubusercontent.com/aeternity/aeternity/master/test/contracts/MainStaking.aes";
 
 const genValidatorName = ({ seed }: { seed?: number | string }) =>
   uniqueNamesGenerator({
@@ -22,30 +31,49 @@ const genValidatorName = ({ seed }: { seed?: number | string }) =>
 
 interface Validator {
   name: string;
-  mnemonic: string;
-  privKey: string;
-  addr: string;
+  account: AccountWithSecrets;
 }
 
-const genValidator = ({
-  nameSeed,
-}: {
-  nameSeed?: number | string;
-}): Validator => {
-  const name = genValidatorName({ seed: nameSeed });
-  const mnemonic = bip39.generateMnemonic();
-  const secret = bip39.mnemonicToSeedSync(mnemonic);
-  const acc = getHdWalletAccountFromSeed(secret, 0);
+function genValidator({ nameSeed }: { nameSeed?: number | string }): Validator {
   return {
-    name,
-    mnemonic,
-    privKey: acc.secretKey,
-    addr: acc.publicKey,
+    name: genValidatorName({ seed: nameSeed }),
+    account: genAccount(),
   };
-};
+}
 
 const genValidators = (count: number) =>
-  Array.from({ length: count }).map((x, n) => genValidator({ nameSeed: n }));
+  Array.from({ length: count }).map((x, n) =>
+    genValidator({ nameSeed: n + 1 })
+  );
 
-const validators = genValidators(3);
-console.log("validators", validators);
+async function fetchContractSource(url: string): Promise<string> {
+  return (await axios.get(url)).data.trim().toString();
+}
+
+async function main() {
+  const validators = genValidators(3);
+  console.log("validators", validators);
+
+  const stakingValidatorContrAddr = aesdk.encodeContractAddress(OWNER_ADDR, 1);
+  console.log("stakingValidatorContrAddr", stakingValidatorContrAddr);
+  const svContract = await genContractDef("master", "StakingValidator", 1, [
+    OWNER_ADDR,
+  ]);
+  console.log(svContract);
+
+  const mainStakingContrAddr = aesdk.encodeContractAddress(OWNER_ADDR, 2);
+  console.log("mainStakingContrAddr", mainStakingContrAddr);
+  const msContract = await genContractDef("master", "MainStaking", 2, [
+    stakingValidatorContrAddr,
+    "entropy_string",
+    0,
+    0,
+    0,
+    0,
+  ]);
+  console.log(msContract);
+}
+
+main().then(() => {
+  // console.log("finished");
+});
