5468201132908ba070b068a8fb0556302f1addec
Metin Akat <akat.metin@gmail.com>
2022-08-29 22:07:22 +0300

    Refactoring: get github settings from config

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,29 +1,39 @@
 import octokit from "octokit";
+import { ContractFile } from "./contracts";
+import z from "zod";
 
 const oct = new octokit.Octokit();
 
-const AeternityMainRepo = { owner: "aeternity", repo: "aeternity" };
+export const GitRepo = z.object({
+  owner: z.string(),
+  repo: z.string(),
+  branch: z.string(),
+  ref: z.string(),
+});
+export type GitRepo = z.infer<typeof GitRepo>;
 
-export async function getRef(branch: string) {
-  console.log("branch", branch);
+export async function getRef(grepo: GitRepo): Promise<GitRepo> {
+  if (grepo.ref != "HEAD") {
+    return grepo;
+  }
+  const { owner, repo, branch } = grepo;
   const res = await oct.rest.git.getRef({
-    ...AeternityMainRepo,
+    owner,
+    repo,
     ref: `heads/${branch}`,
   });
-  return res.data.object.sha;
+  console.log("retrieved ref for branch", branch, res.data.object.sha);
+  return { ...grepo, ref: res.data.object.sha };
 }
 
 export async function retrieveContractSource(
-  name: "MainStaking" | "StakingValidator",
-  branch: string,
-  ref: string
+  repo: GitRepo,
+  fileName: ContractFile
 ): Promise<string> {
   const res = await oct.rest.repos.getContent({
-    ...AeternityMainRepo,
+    ...repo,
     mediaType: { format: "raw" },
-    branch,
-    ref,
-    path: `test/contracts/${name}.aes`,
+    path: `test/contracts/${fileName}`,
   });
   // console.log("res", res.data);
   // return { source: res.data, ref: res.data.ref };
